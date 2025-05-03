b46c3ce33c7b2c5dcb0735d7f0e32ad004085daf
Moisés Jorge <moisesjorge30@gmail.com>
2025-05-03 03:50:27 +0100

    Criando grupos de rotas

--- routes/web.php
+++ routes/web.php
@@ -17,32 +17,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-/* Route::get('/empresa', function () { // Criacao de rotas comuns
-    return view('site/empresa'); 
-}); ESSA ROTA FOI ESCRITA DE FORMA MAIS FACIL NA LINHA 40*/
-
-Route::any('/any', function() { // Any: permite todo tipo de acesso http (get, post, put, delete...)
-    return "Estou aqui porque fui chamado, tenho passe livre na rota Any";
-});
-
-Route::match(['put', 'post'],'/match', function() { // Match: permite apenas acessos definidos
-    return "Estou aqui porque tenho permissao da rota Match";
-});
-
-// Os valores entre chaves depois da rota sao os "parametros"
-Route::get('/produto/{id}/{cat?}', function($id_prod, $categoria = '') {
-    return "O id do produto eh: ".$id_prod."<br>"."A categorai eh: ".$categoria;
-});
-
-Route::redirect('/sobre', '/empresa');
-
-// Se quando uma rota for chamada, tiver que renderizar apenas uma view; entao pode ser escrito de forma mais facil:
-Route::view('/empresa', 'site/empresa');
-
-Route::get('/news', function() {
-    return view('news');
-}) -> name('noticias'); // 'noticias' eh o nome da minha rota /news
-
-Route::get('/novidades', function() {
-    return redirect() -> route('noticias'); // Redirecionando a rota "/novidades" para a rota "/news", mas usando o nome que atribui a rota.
+Route::prefix('admin') -> group(function() {
+    Route::get('dashboard', function() {
+        return 'dashboard';
+    });
+    
+    Route::get('users', function() {
+        return 'users';
+    });
+    
+    Route::get('clientes', function() {
+        return 'clientes';
+    });
 });
